ac9a8a054a8c24fd7f60816e4b09f60d11afce6f
Tigradcom <tigradcom2@gmail.com@>
2022-06-13 11:42:03 +0400

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,5 @@
 public class BonusMilesService {
-        public int calculate (int cost) {
-            return cost / 20;
-
+    public int calculate(int cost) {
+        return cost / 20;
     }
 }
